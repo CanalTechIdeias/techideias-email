c9e8cc58ead525ef1721049c309cc59a65f86bfd
PedroEduardoSS <pedroeduardossousa@gmail.com>
2026-04-15 18:19:18 -0300

    Add dearpygui as a GUI dependency

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import argparse
+import dearpygui.dearpygui as dpg
 from mail import TechIdeiasMail
 
-email = TechIdeiasMail("", "")
+dpg.create_context()
+dpg.create_viewport(title="TechIdeiasMail", width=500, height=500)
+dpg.setup_dearpygui()
 
-def main():
-    parser = argparse.ArgumentParser(description="A program to send emails")
+with dpg.window(label="TechIdeiasMail", tag="TechIdeiasMail"):
+    dpg.add_text("Email")
+    dpg.add_input_text(tag="sender_email")
+    dpg.add_text("App Password")
+    dpg.add_input_text(tag="app_password")
+    email = TechIdeiasMail(f"{dpg.get_value('sender_email')}", f"{dpg.get_value('app_password')}")
 
-
-main()
+dpg.show_viewport()
+dpg.set_primary_window("TechIdeiasMail", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
